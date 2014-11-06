bc07876d5acdb7b4c32bae22c94c9a8bbf35c6a7
griffinl <lq0987@126.com>
2014-11-06 14:20:55 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,4 @@ git pull
 mvn clean package -DskipTests
 
 #cp war to tomcat
-cp target/odin-ad-decision.war /home/hadoop/catalina/apache-tomcat-7.0.52.odin/webapps/
+cp target/odin-ad-decision.war /home/hadoop/catalina/apache-tomcat-7.0.56.odin/webapps/
