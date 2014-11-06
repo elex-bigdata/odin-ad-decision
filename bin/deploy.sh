a04664fbe800f1885121d24851eef4e132938699
griffinl <lq0987@126.com>
2014-11-06 16:44:00 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,4 +3,4 @@ git pull
 mvn clean package -DskipTests
 
 #cp war to tomcat
-cp target/thor.war /home/elex/catalina/apache-tomcat-7.0.56-odin/webapps/
+cp target/thor.war /home/elex/catalina/apache-tomcat-7.0.56-thor/webapps/
