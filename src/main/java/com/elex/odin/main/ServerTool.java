e4226ea518e36c35b6dd17786dea9834e84b3326
griffinl <lq0987@126.com>
2014-11-08 14:22:18 +0800

    update

---
+++ src/main/java/com/elex/odin/main/ServerTool.java
@@ -0,0 +1,20 @@
+package com.elex.odin.main;
+import com.elex.odin.service.FeatureModelService;
+
+/**
+ * Author: liqiang
+ * Date: 14-11-8
+ * Time: 上午10:56
+ */
+public class ServerTool {
+
+    public static void main(String[] args) throws Exception {
+        if(args.length == 0){
+            System.out.println("day");
+            System.exit(-1);
+        }
+        String day = args[0];
+        new FeatureModelService().updateModel(day);
+    }
+
+}
