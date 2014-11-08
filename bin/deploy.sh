61454a994d95465a45590ae9f001f0c3b8ad221a
griffinl <lq0987@126.com>
2014-11-08 14:33:47 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,4 +7,4 @@ rm -rf web/WEB-INF/classes/*
 mvn clean package -DskipTests
 
 #cp war to tomcat
-cp target/thor.war /home/elex/catalina/apache-tomcat-7.0.56-thor/webapps/
+cp target/thor.war /home/elex/webserver/apache-tomcat-7.0.56-thor/webapps/
