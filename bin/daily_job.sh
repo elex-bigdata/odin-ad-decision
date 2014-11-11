21ff60eaec3dead8fd0724d6270763b48e430f47
griffinl <lq0987@126.com>
2014-11-11 11:29:10 +0800

    update

--- bin/daily_job.sh
+++ bin/daily_job.sh
@@ -5,8 +5,7 @@ day=`date -d "1 days ago" +%Y%m%d`
 model_file=/data/odin_model/${day}.gz2
 if [ -f $model_file ]; then
   cd /data/odin_model && tar -xjf $model_file
-  curl "http://127.0.0.1:8080/tool?action=model"
-else
-  echo "${model_file} not found"
 fi
+
+curl "http://127.0.0.1:8080/thor/tool?action=model"
 
