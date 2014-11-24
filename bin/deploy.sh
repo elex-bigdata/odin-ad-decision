e7c824c324fac8b72fbaa5cb937ea01dced288ec
griffinl <lq0987@126.com>
2014-11-24 18:29:38 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -3,9 +3,12 @@ git pull
 
 #clean the cache
 rm -rf web/WEB-INF/classes/*
-rm -rf /home/elex/webserver/apache-tomcat-7.0.56-thor/work/*
 
 mvn clean package -DskipTests
 
+#backup
+
 #cp war to tomcat
+rm -rf /home/elex/webserver/apache-tomcat-7.0.56-thor/work/*
+rm -rf /home/elex/webserver/apache-tomcat-7.0.56-thor/webapps/thor
 cp target/thor.war /home/elex/webserver/apache-tomcat-7.0.56-thor/webapps/
