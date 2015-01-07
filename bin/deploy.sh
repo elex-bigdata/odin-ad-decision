af6e00cd6b6d6035c467a2c5cf4fafffa29e98cb
griffinl <lq0987@126.com>
2015-01-07 11:46:05 +0800

    update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -9,11 +9,11 @@ mvn clean package -DskipTests
 thorpid=`ps aux | grep  '/home/elex/webserver/apache-tomcat-7.0.56-thor' |grep -v grep | awk '{print $2}'`
 
 echo "kill thor thorpid"
-`kill -9 thorpid`
+kill -9 $thorpid
 
 #cp war to tomcat
 rm -rf /home/elex/webserver/apache-tomcat-7.0.56-thor/work/*
 rm -rf /home/elex/webserver/apache-tomcat-7.0.56-thor/webapps/thor
 cp target/thor.war /home/elex/webserver/apache-tomcat-7.0.56-thor/webapps/
 
-`/home/elex/webserver/apache-tomcat-7.0.56-thor/bin/startup.sh`
+/home/elex/webserver/apache-tomcat-7.0.56-thor/bin/startup.sh
